e10006dc770bccfc82d37571498276b66f441a85
ahslxjvhapt12 <likjhee1@gmail.com>
2023-08-16 21:56:10 +0900

    좌표 정렬하기

---
+++ BaekJoon/BaekJoon/백준11650.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<pair<int, int>> v;
+
+	int a, b;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+		v.push_back(make_pair(a, b));
+	}
+
+	sort(v.begin(), v.end());	
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << v[i].first << ' ' << v[i].second << '\n';
+	}
+}
