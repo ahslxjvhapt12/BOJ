32be990be3d59d0541a3f85f0b88256d4f7e2202
ahslxjvhapt12 <likjhee1@gmail.com>
2023-08-17 23:59:40 +0900

    요세푸스 문제

---
+++ BaekJoon/BaekJoon/백준11866.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, k;
+	cin >> n >> k;
+
+	cout << "<";
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 1; j < k; j++) {	//k-1번 동안 front 원소 옮기기
+			q.push(q.front());
+			q.pop();
+		}
+		if (i == n - 1) {
+			cout << q.front() << ">";
+			break;
+		}
+		cout << q.front() << ", ";	//삭제 전 삭제하는 원소 출력
+		q.pop();
+	}
+	return 0;
+}
