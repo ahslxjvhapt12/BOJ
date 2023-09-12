c5ca2ac0ab41002f72f133219ccae440389e93b8
ahslxjvhapt12 <likjhee1@gmail.com>
2023-09-12 17:53:53 +0900

    목표숫자머시기

--- BaekJoon/BaekJoon/0연습장.cpp
+++ BaekJoon/BaekJoon/0연습장.cpp
@@ -1,33 +1,23 @@
+#include <string>
 #include <vector>
+
 using namespace std;
-
 int answer = 0;
-int targetNum;
-int visited[51]{ 0 };
-vector<int> v;
-
-void DFS(int n, char sign) {
-	if (sign == '+') {
-
-	}
-	else {
-
-	}
-
-	if (n + 1 == v.size()) {
-		if (n == targetNum)
-			answer++;
-		return;
-	}
-
-	DFS(n + 1, '+');
-	DFS(n + 1, '-');
+void DFS(int num, int deep, const vector<int>& numbers, const int target)
+{
+    if (deep == numbers.size())
+    {
+        if (num == target)
+            answer++;
+    }
+    else
+    {
+        DFS(num + numbers[deep], deep + 1, numbers, target);
+        DFS(num - numbers[deep], deep + 1, numbers, target);
+    }
 }
 
 int solution(vector<int> numbers, int target) {
-	targetNum = target;
-	v = numbers;
-	DFS(0, '+');
-	DFS(0, '-');
-	return answer;
+    DFS(0, 0, numbers, target);
+    return answer;
 }
