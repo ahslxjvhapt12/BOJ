0d5f61ae7a7492198fd2d01d9bf821a50dbf8230
ahslxjvhapt12 <likjhee1@gmail.com>
2023-11-28 11:27:50 +0900

    Fibo

--- BaekJoon/BaekJoon/0연습장.cpp
+++ BaekJoon/BaekJoon/0연습장.cpp
@@ -1,46 +1,16 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int binary_search(vector<int>& arr, int len, int target) {
-	int left = 0, right = len - 1;
-
-	while (left <= right) {
-		int mid = (left + right) / 2;
-
-		if (target == arr[mid])	return 1;
-
-		if (target < arr[mid]) {
-			right = mid - 1;
-		}
-		else if (target > arr[mid]) {
-			left = mid + 1;
-		}
-	}
-	return 0;
+int Fibo(int n) {
+	if (n == 1 || n == 2) return 1;
+	return Fibo(n - 1) + Fibo(n - 2);
 }
 
 int main() {
 	int n;
-	int input;
-	vector<int> v;
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> input;
-		v.push_back(input);
-	}
-
-	sort(v.begin(), v.end());
-
-	cin >> n;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> input;
-		cout << binary_search(v, v.size(), input) << '\n';
-	}
+	cout << Fibo(n);
+	return 0;
 }
