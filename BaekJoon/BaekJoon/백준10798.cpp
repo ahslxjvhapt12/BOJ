3bae72ed3c6868dc1ba04a4360c5efb49f6fd060
ahslxjvhapt12 <likjhee1@gmail.com>
2023-12-26 09:27:47 +0900

    2차원배열

--- BaekJoon/BaekJoon/백준10798.cpp
+++ BaekJoon/BaekJoon/백준10798.cpp
@@ -1,11 +1,21 @@
-#include<iostream>
-
+#include <iostream>
+#include <cmath>
 using namespace std;
+int main()
+{
+	string s[5];
+	for (int i = 0; i < 5; ++i)
+	{
+		cin >> s[i];
+	}
 
-int main() {
-	int arr[15][15] = {};
-
-	
-
+	for (int j = 0; j < 15; ++j)
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			if (j >= s[i].size()) continue;
+				cout << s[i][j];
+		}
+	}
 	return 0;
 }
